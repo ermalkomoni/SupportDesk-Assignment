9d3ce8e8cc558e251a5771b40a1dd1b37a2093e4
ermalkomoni <ermalkk14@gmail.com>
2026-09-03 01:39:31 +0200

    adding entities

---
+++ SupportDesk.Core/Entities/Comment.cs
@@ -0,0 +1,10 @@
+﻿namespace SupportDesk.Core.Entities;
+
+public class Comment : BaseEntity
+{
+	public string AuthorName { get; set; } = default!;
+	public string Body { get; set; } = default!;
+	public DateTime CreatedDate { get; set; }
+	public Guid TicketId { get; set; }
+	public Ticket Ticket { get; set; } = default!;
+}
