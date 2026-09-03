5094a7e83c34f4a7bb0733c58b2012dc9460c74c
ermalkomoni <ermalkk14@gmail.com>
2026-09-03 15:28:14 +0200

    added ticket queries

---
+++ SupportDesk.Application/Queries/Tickets/GetTicketByIdQuery.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+using SupportDesk.Application.DTOs.Tickets;
+
+namespace SupportDesk.Application.Queries.Tickets;
+
+public record GetTicketByIdQuery(Guid Id) : IRequest<TicketDetailDto?>;
+
