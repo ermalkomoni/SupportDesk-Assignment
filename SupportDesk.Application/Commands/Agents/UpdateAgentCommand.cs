7a6442e347fae25ba26d60b1149cb34d02355885
ermalkomoni <ermalkk14@gmail.com>
2026-09-03 12:24:21 +0200

    agent commands

---
+++ SupportDesk.Application/Commands/Agents/UpdateAgentCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using SupportDesk.Application.DTOs.Agents;
+
+namespace SupportDesk.Application.Commands.Agents;
+
+public record UpdateAgentCommand(Guid Id, UpdateAgentDto Agent) : IRequest<AgentDto>;
