0f169fa994695f67973ec0f4baaaa93d0814d534
ermalkomoni <ermalkk14@gmail.com>
2026-09-03 13:07:14 +0200

    handling apiversioning, swagger ui, dependencyinjection

--- SupportDesk.API/Program.cs
+++ SupportDesk.API/Program.cs
@@ -1,21 +1,43 @@
+using Asp.Versioning;
 using Microsoft.EntityFrameworkCore;
+using SupportDesk.API.Middleware;
+using SupportDesk.Application;
 using SupportDesk.Infrastructure;
 using SupportDesk.Infrastructure.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddApplicationServices();
 builder.Services.AddInfrastructureServices(builder.Configuration);
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+builder.Services.AddApiVersioning(options =>
+	{
+		options.DefaultApiVersion = new ApiVersion(1, 0);
+		options.AssumeDefaultVersionWhenUnspecified = true;
+		options.ReportApiVersions = true;
+	})
+	.AddApiExplorer(options =>
+	{
+		options.GroupNameFormat = "'v'VVV";
+		options.SubstituteApiVersionInUrl = true;
+	});
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+builder.Services.AddProblemDetails();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.UseExceptionHandler();
+
 if (app.Environment.IsDevelopment())
 {
-	app.MapOpenApi();
+	app.UseSwagger();
+	app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
