5d35c02c2d3a85fbae60b4ce83f471db15c6e5d6
ermalkomoni <ermalkk14@gmail.com>
2026-09-03 12:25:35 +0200

    proper handling of exceptions and errors

---
+++ SupportDesk.Application/Exceptions/NotFoundException.cs
@@ -0,0 +1,7 @@
+﻿namespace SupportDesk.Application.Exceptions;
+
+public class NotFoundException : Exception
+{
+	public NotFoundException(string entityName, object key)
+		: base($"{entityName} with id '{key}' was not found.") { }
+}
---
+++ SupportDesk.Core/Exceptions/BusinessRuleException.cs
@@ -0,0 +1,6 @@
+﻿namespace SupportDesk.Core.Exceptions;
+
+public class BusinessRuleException : Exception
+{
+	public BusinessRuleException(string message) : base(message) { }
+}
