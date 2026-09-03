db1f795f96d99170c0cdd02735f58a1c0614a5b3
ermalkomoni <ermalkk14@gmail.com>
2026-09-03 11:50:55 +0200

    adding pagination

---
+++ SupportDesk.Core/Specs/Pagination.cs
@@ -0,0 +1,21 @@
+﻿namespace SupportDesk.Core.Specs;
+
+public class Pagination<T> where T : class
+{
+	public IReadOnlyList<T> Items { get; }
+	public int PageNumber { get; }
+	public int PageSize { get; }
+	public int TotalCount { get; }
+	public int TotalPages { get; }
+	public bool HasPrevious => PageNumber > 1;
+	public bool HasNext => PageNumber < TotalPages;
+
+	public Pagination(IReadOnlyList<T> items, int pageNumber, int pageSize, int totalCount, int totalPages)
+	{
+		Items = items;
+		PageNumber = pageNumber;
+		PageSize = pageSize;
+		TotalCount = totalCount;
+		TotalPages = totalPages;
+	}
+}
