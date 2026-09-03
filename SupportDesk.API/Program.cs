07281316e3543ae80ffb3e738bc0c43ad4387862
ermalkomoni <ermalkk14@gmail.com>
2026-09-03 03:45:21 +0200

    adding migrations and proper setting up db

--- SupportDesk.API/Program.cs
+++ SupportDesk.API/Program.cs
@@ -1,6 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using SupportDesk.Infrastructure.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+				options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -19,5 +23,11 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+using (var scope = app.Services.CreateScope())
+{
+	var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+	await context.Database.MigrateAsync();
+}
 
 app.Run();
